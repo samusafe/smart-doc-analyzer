f6f5fdbc73ac68187247cf17559339e71ec77e85
samusafe <samucarvalhocp@gmail.com>
2025-09-03 00:37:23 +0100

    swagger update + frontend volume creation

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
